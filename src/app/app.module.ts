61175c1ef0f7da52d199ce81034590e2bff656b7
Jepphire <90650560+Jepphire@users.noreply.github.com>
2021-12-13 19:13:27 -0600

    created random-joke component
    created joke service to call a random joke from chuck norris api based on a joke model, used string interpolation to dynamically display joke value and icon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RandomJokeComponent } from './random-joke/random-joke.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RandomJokeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
